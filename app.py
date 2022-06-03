24d4801623f44f4a3c5289d997c2a86d5db393b7
Flow-matic <jonathonvinson22@gmail.com>
2022-06-03 17:58:03 +0000

    Changed button

--- app.py
+++ app.py
@@ -93,4 +93,3 @@ st.markdown("""
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 """, unsafe_allow_html=True)
 
-st.button('Hit me')
